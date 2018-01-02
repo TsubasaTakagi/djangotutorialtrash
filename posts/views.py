15367a1b415a45ef36ed5e1a4d48eaf2e92fea9c
TsubasaTakagi <mizuirotech@gmail.com>
2018-01-02 16:32:52 +0900

    post

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+
+def index(request):
+    #return HttpResponse("Hello World! このページは投稿のインデックスです。")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
